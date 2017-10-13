196be9cb47a7e600bc6ccfc5ab144b1b383bbf12
Carey Metcalfe <carey@cmetcalfe.ca>
2017-10-12 23:25:47 -0400

    Add support for Android Nougat and Oreo
    Fixes #1

--- app/src/main/java/ca/cmetcalfe/xposed/disablebatterywarnings/Main.java
+++ app/src/main/java/ca/cmetcalfe/xposed/disablebatterywarnings/Main.java
@@ -12,6 +12,17 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 
+/*
+Links to relevant AOSP source code:
+
+https://android.googlesource.com/platform/frameworks/base/+/jb-dev/packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+https://android.googlesource.com/platform/frameworks/base/+/kitkat-dev/packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+https://android.googlesource.com/platform/frameworks/base/+/lollipop-dev/packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+https://android.googlesource.com/platform/frameworks/base/+/marshmallow-dev/packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+https://android.googlesource.com/platform/frameworks/base/+/nougat-dev/packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+https://android.googlesource.com/platform/frameworks/base/+/oreo-dev/packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+*/
+
 
 public class Main implements IXposedHookLoadPackage {
 
@@ -22,9 +33,9 @@ public class Main implements IXposedHookLoadPackage {
             return;
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN ||
-                Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             XposedBridge.log("The DisableBatteryWarnings Xposed module will only work on " +
-                    "versions Jelly Bean to Marshmallow - exiting now");
+                    "versions Jelly Bean to Oreo - exiting now");
             return;
         }
 
@@ -37,14 +48,14 @@ public class Main implements IXposedHookLoadPackage {
     }
 
     private void applyHooks(LoadPackageParam loadPackageParam) throws Throwable {
-        // Jelly Bean and KitKat
+        // Jelly Bean, KitKat
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             Class<?> powerUI = findClass("com.android.systemui.power.PowerUI",
                     loadPackageParam.classLoader);
 
             findAndHookMethod(powerUI, "playLowBatterySound", new XC_MethodReplacement() {
                 @Override
-                protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
                     return null;
                 }
             });
@@ -56,7 +67,7 @@ public class Main implements IXposedHookLoadPackage {
                 }
             });
         }
-        // Lollipop and Marshmallow
+        // Lollipop, Marshmallow, Nougat, Oreo
         else {
             Class<?> powerNotificationWarnings = findClass("com.android.systemui.power.PowerNotificationWarnings",
                     loadPackageParam.classLoader);
@@ -64,7 +75,6 @@ public class Main implements IXposedHookLoadPackage {
             findAndHookMethod(powerNotificationWarnings, "updateNotification", new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedHelpers.setBooleanField(param.thisObject, "mPlaySound", false);
                     XposedHelpers.setBooleanField(param.thisObject, "mWarning", false);
                 }
             });
